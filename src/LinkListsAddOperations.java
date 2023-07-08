fa6f462d589c41b6b50507fd4873aa8c0f467302
SAHIL BANSAL <BANSALSAHIL9053@GMAIL.COM>
2023-07-08 12:33:33 +0530

    linklist

--- src/LinkListsAddOperations.java
+++ src/LinkListsAddOperations.java
@@ -8,7 +8,7 @@
 //                            doubly linked (each node has references to the next and previous nodes), or
 //                            circular (the last node points back to the head node).
 
-public class LinkLists {
+public class LinkListsAddOperations {
 
     public static class Node {
         int data;
@@ -22,10 +22,12 @@ public class LinkLists {
 
     public static Node head;
     public static Node tail;
+    public static int size;
 
     public void addFirst(int data){   // Time Complexity = O(1)
         // Create New Node
         Node newNode = new Node(data);
+        size++;
         if (head == null){
             head = tail = newNode;
             return;
@@ -36,12 +38,30 @@ public class LinkLists {
 
     public void addLast(int data) {  // Time Complexity = O(1)
         Node newNode = new Node(data);
+        size++;
         if (head == null){
             head = tail = newNode;
             return;
         }
         tail.next = newNode;
         tail = newNode;
+    }
+
+    public void addMiddle(int data, int index) {
+        if (index == 0) {
+            addFirst(data);
+            return;
+        }
+        Node newNode = new Node(data);
+        size++;
+        int i = 0;
+        Node temp = head;
+        while (i < index - 1){
+            temp = temp.next;
+            i++;
+        }
+        newNode.next = temp.next;
+        temp.next = newNode;
     }
 
     public static void print(){
@@ -56,29 +76,29 @@ public class LinkLists {
 
         System.out.println();
     }
+//   public static void Size(){
+//        int size = 0;
+//        if (head == null){
+//            System.out.println("Size of list is: " + size);
+//        }
+//        Node temp = head;
+//        while (temp != null){
+//            temp = temp.next;
+//            size++;
+//        }
+//        System.out.println(size);
+//    }
 
 
     public static void main(String[] args) {
 
-        LinkLists list = new LinkLists();
-//        list.addFirst(1);
-//        list.addFirst(2);
-//        print();
+        LinkListsAddOperations list = new LinkListsAddOperations();
         list.addLast(1);
         list.addLast(2);
         list.addLast(3);
         list.addFirst(4);
+        list.addMiddle(5, 2);
         print();
-//        list.head.next = new Node(2);
-//        list.addFirst("1");
-//        list.addFirst("2");
-//        list.addFirst("3");
-//        list.addLast("5");
-//        list.addLast("6");
-//        list.printLL();
-//        list.deleteFirst();
-//        list.printLL();
-//        list.deleteLast();
-//        list.printLL();
+        System.out.println(list.size);
     }
 }
